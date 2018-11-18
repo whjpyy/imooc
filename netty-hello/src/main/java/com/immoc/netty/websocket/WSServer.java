6575235f981e15785884fe2a9a6c4cafbb276010
tianma <tianma@maike51.com>
2018-11-18 22:52:54 +0800

    netty编写微信：第4章-Netty构建websocket服务器

---
+++ netty-hello/src/main/java/com/immoc/netty/websocket/WSServer.java
@@ -0,0 +1,28 @@
+package com.immoc.netty.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class WSServer {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup subGroup = new NioEventLoopGroup();
+
+        try{
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(mainGroup, subGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new WSServerInitializer());
+
+            ChannelFuture future = server.bind(8088).sync();
+
+            future.channel().closeFuture().sync();
+        } finally {
+            mainGroup.shutdownGracefully();
+            subGroup.shutdownGracefully();
+        }
+    }
+}
